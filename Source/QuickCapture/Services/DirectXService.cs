01a45853865ac7085488f7e7c69e02d6d34188bb
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-24 21:45:38 +0900

    feat: add DirectX service (as singleton)

---
+++ Source/QuickCapture/Services/DirectXService.cs
@@ -0,0 +1,17 @@
+﻿using QuickCapture.Services.Interfaces;
+
+using SharpDX.Direct3D;
+using SharpDX.Direct3D11;
+
+namespace QuickCapture.Services
+{
+    internal class DirectXService : IDirectXService
+    {
+        public Device Device { get; private set; }
+
+        public void Initialize()
+        {
+            Device = new Device(DriverType.Hardware, DeviceCreationFlags.BgraSupport);
+        }
+    }
+}
---
+++ Source/QuickCapture/Services/Interfaces/IDirectXService.cs
@@ -0,0 +1,11 @@
+﻿using SharpDX.Direct3D11;
+
+namespace QuickCapture.Services.Interfaces
+{
+    internal interface IDirectXService
+    {
+        Device Device { get; }
+
+        void Initialize();
+    }
+}
