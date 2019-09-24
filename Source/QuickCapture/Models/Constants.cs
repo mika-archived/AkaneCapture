67b2d96ed07ff2e01cfc6df7fad54990ef51b869
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-24 21:45:03 +0900

    chore: add application constants

---
+++ Source/QuickCapture/Models/Constants.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.IO;
+
+namespace QuickCapture.Models
+{
+    internal static class Constants
+    {
+        public static string ApplicationDir => Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData), "mochizuki.moe", "QuickCapture");
+
+        public static string HistoryFilePath => Path.Combine(ApplicationDir, "histories.json");
+        public static string SituationsDirPath => Path.Combine(ApplicationDir, "situations");
+    }
+}
