20fde3d5e85ee8a970476ca515e53657a00a550a
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-25 00:15:04 +0900

    feat: show tracking configuration

---
+++ Source/QuickCapture/Views/Partials/Configurations.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace QuickCapture.Views.Partials
+{
+    /// <summary>
+    ///     Configurations.xaml の相互作用ロジック
+    /// </summary>
+    public partial class Configurations : UserControl
+    {
+        public Configurations()
+        {
+            InitializeComponent();
+        }
+    }
+}
