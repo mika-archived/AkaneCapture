db1493eb44d2f13e4f1aecd81877a80c7a7b2624
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-24 21:27:20 +0900

    feat: dispose CompositeDisposable on closing app

---
+++ Source/QuickCapture/Actions/DataContextDisposeAction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Interactivity;
+
+namespace QuickCapture.Actions
+{
+    internal class DataContextDisposeAction : TriggerAction<FrameworkElement>
+    {
+        protected override void Invoke(object parameter)
+        {
+            var disposable = AssociatedObject.DataContext as IDisposable;
+            disposable?.Dispose();
+        }
+    }
+}
