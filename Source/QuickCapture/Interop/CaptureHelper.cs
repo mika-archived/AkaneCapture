07f5636606d753a63cd708bff09bf4bcd2c147f9
Mikazuki <mikazuki_fuyuno@outlook.com>
2019-09-25 00:13:08 +0900

    feat: use WinRT APIs for capturing process

---
+++ Source/QuickCapture/Interop/CaptureHelper.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.WindowsRuntime;
+
+using Windows.Graphics.Capture;
+
+using QuickCapture.Interop.WinRT;
+
+namespace QuickCapture.Interop
+{
+    internal static class CaptureHelper
+    {
+        // ReSharper disable once SuspiciousTypeConversion.Global
+        public static GraphicsCaptureItem CreateItemForWindow(IntPtr hWnd)
+        {
+            var factory = WindowsRuntimeMarshal.GetActivationFactory(typeof(GraphicsCaptureItem));
+            var interop = (IGraphicsCaptureItemInterop) factory;
+            var iid = typeof(GraphicsCaptureItem).GetInterface("IGraphicsCaptureItem").GUID;
+            var pointer = interop.CreateForWindow(hWnd, ref iid);
+            var capture = Marshal.GetObjectForIUnknown(pointer) as GraphicsCaptureItem;
+            Marshal.Release(pointer);
+
+            return capture;
+        }
+    }
+}
---
+++ Source/QuickCapture/Interop/WinRT/IGraphicsCaptureItemInterop.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace QuickCapture.Interop.WinRT
+{
+    [ComImport]
+    [Guid("3628E81B-3CAC-4C60-B7F4-23CE0E0C3356")]
+    [InterfaceType(ComInterfaceType.InterfaceIsIUnknown)]
+    internal interface IGraphicsCaptureItemInterop
+    {
+        IntPtr CreateForWindow([In] IntPtr hWnd, ref Guid iid);
+
+        IntPtr CreateForMonitor([In] IntPtr hMonitor, ref Guid iid);
+    }
+}
